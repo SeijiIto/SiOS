838c1a6997e6b294b22aaa256c0f0d7e09aa0ea1
Dominik Wagner <dom@codingmonkeys.de>
2014-03-15 12:47:55 +0100

    [CHANGE] optimized setup for controller buttons (using a callback per button instead of a general callback that switches around, also made the left analog stick of the extended controller send the dpad commands (should have worked with the old code too mut somehow didn't)

--- Classes/LMGameControllerManager.m
+++ Classes/LMGameControllerManager.m
@@ -18,32 +18,15 @@
 
 - (void)LM_setupController
 {
-  if([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0f)
-  {
-    NSArray* controllers = [GCController controllers];
-    // Grab first controller
-    // TODO: Add support for multiple controllers
-    _gameController = [controllers firstObject];
-	_gameController.playerIndex = 0;
-    __weak id weakSelf = self;
-    
-    _gameController.gamepad.valueChangedHandler = ^(GCGamepad* gamepad, GCControllerElement* element) {
-      [weakSelf LM_getCurrentControllerInput];
-    };
-    _gameController.extendedGamepad.valueChangedHandler = ^(GCExtendedGamepad* gamepad, GCControllerElement* element) {
-      [weakSelf LM_getCurrentControllerInput];
-    };
-	  _gameController.controllerPausedHandler = ^(GCController *controller) {
-		  SEL optionsSelector = @selector(LM_options:);
-		  UIViewController *topViewController = [[[[UIApplication sharedApplication] windows] firstObject] rootViewController];
-		  while (topViewController.presentedViewController) {
-			  topViewController = topViewController.presentedViewController;
-		  }
-		  if ([topViewController respondsToSelector:optionsSelector]) {
-			  [topViewController performSelector:optionsSelector withObject:nil afterDelay:0.0];
-		  }
-	  };
-  }
+	if([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0f)
+	{
+		NSArray* controllers = [GCController controllers];
+		// Grab first controller
+		// TODO: Add support for multiple controllers
+		_gameController = [controllers firstObject];
+		
+		[self LM_setupController:_gameController forPlayerIndex:0];
+	}
 }
 
 - (void)LM_controllerConnected:(NSNotification*)notification
@@ -58,159 +41,70 @@
   [self.delegate gameControllerManagerGamepadDidDisconnect:self];
 }
 
-- (void)LM_getCurrentControllerInput
-{
-  if(_gameController)
-  {
-    if(_gameController.extendedGamepad)
-    {
-      GCExtendedGamepad* extendedGamepad = _gameController.extendedGamepad;
-      
-      // You should swap A+B / X+Y because it feels awkward on Gamepad
-      if (extendedGamepad.buttonA.pressed) {
-        SISetControllerPushButton(kSIOS_1PB);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PB);
-      }
-      if (extendedGamepad.buttonB.pressed) {
-        SISetControllerPushButton(kSIOS_1PA);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PA);
-      }
-      if (extendedGamepad.buttonX.pressed) {
-        SISetControllerPushButton(kSIOS_1PY);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PY);
-      }
-      if (extendedGamepad.buttonY.pressed) {
-        SISetControllerPushButton(kSIOS_1PX);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PX);
-      }
-      
-      if (extendedGamepad.leftShoulder.pressed) {
-        SISetControllerPushButton(kSIOS_1PL);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PL);
-      }
-      
-      if (extendedGamepad.rightShoulder.pressed) {
-        SISetControllerPushButton(kSIOS_1PR);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PR);
-      }
-      
-      // This feels super awkward
-      /*
-       if (extendedGamepad.buttonX.pressed) {
-       padInput |= NestopiaPadInputStart;
-       }
-       if (extendedGamepad.buttonY.pressed) {
-       padInput |= NestopiaPadInputSelect;
-       }
-       */
-      
-		if (extendedGamepad.leftTrigger.pressed) {
-			SISetControllerPushButton(kSIOS_1PSelect);
-		} else {
-			SISetControllerReleaseButton(kSIOS_1PSelect);
-		}
-		if (extendedGamepad.rightTrigger.pressed) {
-			SISetControllerPushButton(kSIOS_1PStart);
-		} else {
-			SISetControllerReleaseButton(kSIOS_1PStart);
-		}
+- (void)LM_setupController:(GCController *)aController forPlayerIndex:(int)aPlayerIndex {
+
+	aController.playerIndex = aPlayerIndex;
+	
+	// callback block creation helper block
+	GCControllerButtonValueChangedHandler (^buttonValueChangeHandlerWithButtonConstant)(int) = ^(int buttonConstant) {
+		buttonConstant += (aPlayerIndex * (kSIOS_2PX - kSIOS_1PX)); // shift it up if not for first player
+		GCControllerButtonValueChangedHandler result = ^(GCControllerButtonInput *button, float value, BOOL pressed) {
+			if (pressed) {
+				SISetControllerPushButton(buttonConstant);
+			} else {
+				SISetControllerReleaseButton(buttonConstant);
+			}
+		};
+		return result;
+	};
+	
+	// helper block to setup up,down,left,right handlers for a element that has these button properties
+	void (^setupDirectionalElement)(id) = ^(id directionalElement){
+		[directionalElement up].valueChangedHandler = buttonValueChangeHandlerWithButtonConstant(kSIOS_1PUp);
+		[directionalElement down].valueChangedHandler = buttonValueChangeHandlerWithButtonConstant(kSIOS_1PDown);
+		[directionalElement left].valueChangedHandler = buttonValueChangeHandlerWithButtonConstant(kSIOS_1PLeft);
+		[directionalElement right].valueChangedHandler = buttonValueChangeHandlerWithButtonConstant(kSIOS_1PRight);
+	};
+	
+	if (aController) {
+		GCGamepad *gamepad = _gameController.gamepad;
 		
-      // Extended Gamepad gets a thumbstick as well
-      if (extendedGamepad.dpad.up.pressed || extendedGamepad.leftThumbstick.up.pressed) {
-        SISetControllerPushButton(kSIOS_1PUp);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PUp);
-      }
-      if (extendedGamepad.dpad.down.pressed || extendedGamepad.leftThumbstick.down.pressed) {
-        SISetControllerPushButton(kSIOS_1PDown);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PDown);
-      }
-      if (extendedGamepad.dpad.left.pressed || extendedGamepad.leftThumbstick.left.pressed) {
-        SISetControllerPushButton(kSIOS_1PLeft);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PLeft);
-      }
-      if (extendedGamepad.dpad.right.pressed || extendedGamepad.leftThumbstick.right.pressed) {
-        SISetControllerPushButton(kSIOS_1PRight);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PRight);
-      }
-    }
-    
-    if(_gameController.gamepad)
-    {
-      GCGamepad* gamepad = _gameController.gamepad;
-      
-      // You should swap A+B / X+Y because it feels awkward on Gamepad
-      if (gamepad.buttonA.pressed) {
-        SISetControllerPushButton(kSIOS_1PB);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PB);
-      }
-      if (gamepad.buttonB.pressed) {
-        SISetControllerPushButton(kSIOS_1PA);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PA);
-      }
-      if (gamepad.buttonX.pressed) {
-        SISetControllerPushButton(kSIOS_1PY);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PY);
-      }
-      if (gamepad.buttonY.pressed) {
-        SISetControllerPushButton(kSIOS_1PX);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PX);
-      }
-      
-      // Extended Gamepad gets a thumbstick as well
-      if (gamepad.dpad.up.pressed) {
-        SISetControllerPushButton(kSIOS_1PUp);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PUp);
-      }
-      if (gamepad.dpad.down.pressed) {
-        SISetControllerPushButton(kSIOS_1PDown);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PDown);
-      }
-      if (gamepad.dpad.left.pressed) {
-        SISetControllerPushButton(kSIOS_1PLeft);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PLeft);
-      }
-      if (gamepad.dpad.right.pressed) {
-        SISetControllerPushButton(kSIOS_1PRight);
-      }
-      else {
-        SISetControllerReleaseButton(kSIOS_1PRight);
-      }
-    }
-  }
+		if (_gameController.extendedGamepad) {
+			gamepad = (id)_gameController.extendedGamepad;
+
+			GCExtendedGamepad* extendedGamepad = _gameController.extendedGamepad;
+			extendedGamepad.leftTrigger.valueChangedHandler = buttonValueChangeHandlerWithButtonConstant(kSIOS_1PSelect);
+			extendedGamepad.rightTrigger.valueChangedHandler = buttonValueChangeHandlerWithButtonConstant(kSIOS_1PStart);
+			setupDirectionalElement(extendedGamepad.leftThumbstick);
+		}
+
+		// You should swap A+B / X+Y because it feels awkward on Gamepad
+		gamepad.buttonA.valueChangedHandler = buttonValueChangeHandlerWithButtonConstant(kSIOS_1PB);
+		gamepad.buttonB.valueChangedHandler = buttonValueChangeHandlerWithButtonConstant(kSIOS_1PA);
+		gamepad.buttonX.valueChangedHandler = buttonValueChangeHandlerWithButtonConstant(kSIOS_1PY);
+		gamepad.buttonY.valueChangedHandler = buttonValueChangeHandlerWithButtonConstant(kSIOS_1PX);
+
+		setupDirectionalElement(gamepad.dpad);
+		
+		gamepad.leftShoulder.valueChangedHandler = buttonValueChangeHandlerWithButtonConstant(kSIOS_1PL);
+		gamepad.rightShoulder.valueChangedHandler = buttonValueChangeHandlerWithButtonConstant(kSIOS_1PR);
+		
+	}
+	
+	// setup pause button
+	
+	_gameController.controllerPausedHandler = ^(GCController *controller) {
+		SEL optionsSelector = @selector(LM_options:);
+		UIViewController *topViewController = [[[[UIApplication sharedApplication] windows] firstObject] rootViewController];
+		while (topViewController.presentedViewController) {
+			topViewController = topViewController.presentedViewController;
+		}
+		if ([topViewController respondsToSelector:optionsSelector]) {
+			[topViewController performSelector:optionsSelector withObject:nil afterDelay:0.0];
+		}
+	};
+
+	
 }
 
 @end
